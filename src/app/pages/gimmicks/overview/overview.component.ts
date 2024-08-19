7075b1016e0e37c50c7a5712f5efb5ab2492ce3b
Benjamin Milčić <benjamin.milcic@gmail.com>
2024-08-19 15:44:25 +0200

    bugfix

--- src/app/pages/gimmicks/overview/overview.component.ts
+++ src/app/pages/gimmicks/overview/overview.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [RouterModule, TranslateModule],
+  imports: [RouterModule, TranslateModule, CommonModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css',
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+  constructor(public translateService: TranslateService) {}
+}
